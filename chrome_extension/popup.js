11101826a0024ca7641ef1f396408278e79bcfc6
smn57 <Gimmini@stud.uni-heidelberg.de>
2019-05-05 18:32:10 +0200

    first tests on popup

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -2,12 +2,37 @@ $(function () {
     $('#name').keyup(function () {
         $('#greet').text('Hello ' + $('#name').val());
     })
-})
+});
 
 $(function () {
-    $('#warning').keyup(function () {
-        $('#warning').text('WHAAAT');
+    $("#warning").click(function () {
+        $(this).hide()
+    })
+
+});
+
+$(function () {
+    $('#input').keyup(function () {
+        $('#output').text($('#input').val());
+    })
+});
+
+
+$(function () {
+    $('.someclass').click(function () {
+        $(this).hide()
+        $("#warning").show().text('Das ist gut');
+        $('#output').text("")
+    })
+});
+
+$(function () {
+    $(document).ready(function () {
+        $('#extract').onclick(function () {
+            $('#output').text($(".lang-py prettyprint prettyprinted").val())
+        })
 
     })
+
 
 })
