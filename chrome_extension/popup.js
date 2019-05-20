17fd6ca429f2a3b40a0f6f1513451bdc6bc4740e
Tobias Neuschäfer <t.neuschaefer@stud.uni-heidelberg.de>
2019-05-20 11:50:42 +0200

    added working native messaging host

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,61 +1,21 @@
-$(function () {
-    $('#name').keyup(function () {
-        $('#greet').text('Hello ' + $('#name').val());
-    })
-});
-
-$(function () {
-    $("#warning").click(function () {
-        $(this).hide()
-    })
-
-});
-
-$(function () {
-    $('#input').keyup(function () {
-        $('#output').text($('#input').val());
-    })
-});
-
-
-$(function () {
-    $('.someclass').click(function () {
-        $(this).hide()
-        $("#warning").show().text('Das ist gut');
-        $('#output').text("")
-    })
-});
-
-$(function () {
-    $(document).ready(function () {
-        $('#extract').click(function () {
-            $('#output').text($(".lang-py prettyprint prettyprinted").val())
-        })
-
-    })
-
-
-});
-
-$(document).ready(function(){
-    $('body').on('click', 'a', function(){
-      chrome.tabs.create({url: $(this).attr('href')});
-      return false;
+$(document).ready(function () {
+    $('body').on('click', 'a', function () {
+        chrome.tabs.create({ url: $(this).attr('href') });
+        return false;
     });
 });
 
 function sendNativeMessage() {
-   message = {
-       "name": document.getElementById('snippetname').value,
-       "prefix": document.getElementById('snippetprefix').value,
-       "body": document.getElementById('snippetbody').value,
-       "description": document.getElementById('snippetdescription').value
-   };
-   chrome.runtime.sendNativeMessage('native.messaging.example', message);
+    message = {
+        "name": document.getElementById('snippetname').value,
+        "prefix": document.getElementById('snippetprefix').value,
+        "body": document.getElementById('snippetbody').value,
+        "description": document.getElementById('snippetdescription').value
+    };
+    chrome.runtime.sendNativeMessage('frag_extract_host', message);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('send-message-button').addEventListener(
         'click', sendNativeMessage);
-    // updateUiState();
 });
---
+++ vs_code_extension/frag_extract_host/uninstall_host.bat
@@ -0,0 +1,7 @@
+@echo off
+:: Copyright 2014 The Chromium Authors. All rights reserved.
+:: Use of this source code is governed by a BSD-style license that can be
+:: found in the LICENSE file.
+
+:: Deletes the entry created by install_host.bat
+REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\frag_extract_host" /f
