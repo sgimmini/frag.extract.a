6728b6dd84f2a9f03fb1e27450a985b81e42936e
Tobias Neuschäfer <t.neuschaefer@stud.uni-heidelberg.de>
2019-05-24 15:54:17 +0200

    failed attempt to translate frag_extrac_host.py to js

--- vs_code_extension/frag_extract_host/frag_extract_host.py
+++ vs_code_extension/frag_extract_host/frag_extract_host.py
@@ -6,9 +6,11 @@ import struct
 # On Windows, the default I/O mode is O_TEXT. Set this to O_BINARY
 # to avoid unwanted modifications of the input/output streams.
 if sys.platform == "win32":
-	import os, msvcrt
-	msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
-	msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+    import os
+    import msvcrt
+    msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
+    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+
 
 # from https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging
 def get_message():
@@ -19,11 +21,13 @@ def get_message():
     message = sys.stdin.buffer.read(message_length).decode("utf-8")
     return json.loads(message)
 
+
 def Main():
-	recieved = get_message()
-	with open('data.json', 'w') as outfile:
-    		json.dump(recieved, outfile)
-	sys.exit(0)
+    recieved = get_message()
+    with open('message.json', 'w') as outfile:
+        json.dump(recieved, outfile)
+    sys.exit(0)
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
---
+++ vs_code_extension/make.sh
@@ -0,0 +1,1 @@
+rapydscript -b -p -m frag_extract_host.py > frag_extract_host.js
