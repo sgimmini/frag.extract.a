65f649701c7ccdb672e470fd5ea2d7a1973a27ba
Tobias Neuschäfer <t.neuschaefer@stud.uni-heidelberg.de>
2019-06-08 18:11:31 +0200

    Removed no longer needed files

--- vs_code_extension/frag.extract.host/install_host.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-:: Change HKCU to HKLM if you want to install globally.
-:: %~dp0 is the directory containing this bat script and ends with a backslash.
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.frag.extract" /ve /t REG_SZ /d "%~dp0win.com.frag.extract.json" /f
--- vs_code_extension/frag_extract_host/uninstall_host.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-:: Deletes the entry created by install_host.bat
-REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.frag.extract" /f
