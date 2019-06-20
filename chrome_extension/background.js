0d7dfb4b6291549925992a5d0c21e9104baa5787
Tobias Neuschäfer <t.neuschaefer@stud.uni-heidelberg.de>
2019-06-21 00:03:35 +0200

    Removed support for local sites

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,17 +1,14 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
 
   // Extension popup menu can only be opened stackoverflow.com and local files
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'stackoverflow.com'},
-          }),
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {urlContains: 'file://'},
-          })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'stackoverflow.com' },
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
 });
