675ec14a8d03ea4fce4a7445948b1122057dad89
Tobias Neuschäfer <t.neuschaefer@stud.uni-heidelberg.de>
2019-06-21 01:29:03 +0200

    added listeners that save every input

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -21,6 +21,7 @@ function sendNativeMessage() {
     window.close();
 }
 
+// send and cancel buttons
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('send_button').addEventListener(
         'click', sendNativeMessage);
@@ -29,4 +30,43 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('cancel_button').addEventListener(
         'click', function () { window.close(); });
+});
+
+// save content of input fields
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('snippetlabel').addEventListener(
+        'input', function () { chrome.storage.local.set({ label: document.getElementById('snippetlabel').value }); });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('snippetprefix').addEventListener(
+        'input', function () { chrome.storage.local.set({ prefix: document.getElementById('snippetprefix').value }); });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('snippetscope').addEventListener(
+        'input', function () { chrome.storage.local.set({ scope: document.getElementById('snippetscope').value }); });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('snippetbody').addEventListener(
+        'input', function () { chrome.storage.local.set({ body: document.getElementById('snippetbody').value }); });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('snippetdescription').addEventListener(
+        'input', function () {
+            chrome.storage.local.set({ description: document.getElementById('snippetdescription').value });
+            chrome.storage.local.get(['label'], function (result) { document.getElementById('snippetprefix').value = result.label; });
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('snippetags').addEventListener(
+        'input', function () { chrome.storage.local.set({ tags: document.getElementById('snippettags').value }); });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('snippetdomain').addEventListener(
+        'input', function () { chrome.storage.local.set({ domain: document.getElementById('snippetdomain').value }); });
 });
