9c5e6c723a71038093dc088a12f878d9b0428738
Tobias Neuschäfer <t.neuschaefer@stud.uni-heidelberg.de>
2019-06-23 11:25:43 +0200

    Made sure that native message is only send when intended

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -18,18 +18,20 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
 });
 
 chrome.runtime.onMessage.addListener(function (recieved, callback) {
-  chrome.storage.local.get({ 'label': "", 'prefix': "", 'scope': "", 'body': "", 'description': "", 'tags': "", 'domain': "" }, function (result) {
-    var message = {};
-    message.label = result.label;
-    message.prefix = result.prefix;
-    message.scope = result.scope;
-    message.body = result.body;
-    message.description = result.description;
-    message.keywords = "";
-    message.tags = result.tags;
-    message.domain = result.domain;
-    message.placeholders = "";
-    chrome.runtime.sendNativeMessage('com.frag.extract', message);
-    chrome.storage.local.remove(['label', 'prefix', 'scope', 'body', 'description', 'tags', 'domain']);
-  });
+  if (recieved.content == 'sendNativeMessage') {
+    chrome.storage.local.get({ 'label': "", 'prefix': "", 'scope': "", 'body': "", 'description': "", 'tags': "", 'domain': "" }, function (result) {
+      var message = {};
+      message.label = result.label;
+      message.prefix = result.prefix;
+      message.scope = result.scope;
+      message.body = result.body;
+      message.description = result.description;
+      message.keywords = "";
+      message.tags = result.tags;
+      message.domain = result.domain;
+      message.placeholders = "";
+      chrome.runtime.sendNativeMessage('com.frag.extract', message);
+      chrome.storage.local.remove(['label', 'prefix', 'scope', 'body', 'description', 'tags', 'domain']);
+    });
+  }
 });
