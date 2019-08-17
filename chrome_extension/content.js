4fefc8c4fe24a780f64b7bad47cc15b625df5c42
Florian Rauls <frauls@web.de>
2019-08-17 13:44:38 +0200

    Moved functions to bottom of content.js

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -11,64 +11,6 @@ const border = " stop "
 // this list is not comprehensive, please add any further programming lanuages you may think of
 const languageList = ['javascript', 'java', 'c#', 'php', 'python', 'html', 'c++', 'css', 'sql', 'c', 'r', 'objective-c', 'swift', 'ruby', 'excel', 'vba', 'vb.net', 'scala', 'typescript', 'matlab', 'bash', 'shell', 'go', 'rust', 'octave'];
 
-// This functions takes an 2d Array as Input and returns the array sorted by its 1st column in descending order
-function sortFunction(a, b) {
-    if (a[0] === b[0]) {
-        return 0;
-    }
-    else {
-        return (a[0] < b[0]) ? -1 : 1;
-    }
-}
-
-// This function takes an URL and makes and HTTP Request
-function Get(yourUrl){
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;          
-}
-
-// This function takes an URL and returns the TensorflowLayersModel which should lie at the other side
-async function create_Model(url){
-    var back = await tf.loadLayersModel(url);
-    return back;
-}
-// This function takes an URL and returns the JSON object which should lie at the other side
-async function create_Vocab(url){
-    var back = JSON.parse(Get(url));
-    return back;
-}
-// this function takes a tokenized string as input and returns a probability of
-// the given intent (tokens before border - token) and
-// and codeblock being a good fit
-function evaluate(seedWord, mod, voc) {
-        // tensor to return later
-        var to_return = new Array(MAX_LEN).fill(0);
-        var length = seedWord.length;
-        // If the word is in our dictionary we assign it it's value
-        // else it gets "deleted" by the offset
-        var offset = 0;
-        for (var i = 0; i < length; i++) {
-            if (voc.hasOwnProperty(seedWord[i])) {
-                to_return[i - offset] = voc[seedWord[i]]
-            }
-            else {
-                offset = offset + 1;
-            }
-        }
-        const shape = [1, MAX_LEN]
-        // calling the model
-        // var ret = tf.loadLayersModel(MODEL_URL).then(model => {
-        const result = mod.predict(tf.tensor(to_return, shape))
-        var resultData = result.dataSync();
-        var back = resultData[0]
-        //     return back;
-        // });
-        return back;
-}
-
-
 // run setup when content script is injected into SO page
 setup();
 
@@ -241,6 +183,66 @@ function detectJsHtml(codeblock) {
         scope = scopeArray[0];
     }
 };
+
+
+
+// This functions takes an 2d Array as Input and returns the array sorted by its 1st column in descending order
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
+}
+
+// This function takes an URL and makes and HTTP Request
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
+// This function takes an URL and returns the TensorflowLayersModel which should lie at the other side
+async function create_Model(url){
+    var back = await tf.loadLayersModel(url);
+    return back;
+}
+// This function takes an URL and returns the JSON object which should lie at the other side
+async function create_Vocab(url){
+    var back = JSON.parse(Get(url));
+    return back;
+}
+// this function takes a tokenized string as input and returns a probability of
+// the given intent (tokens before border - token) and
+// and codeblock being a good fit
+function evaluate(seedWord, mod, voc) {
+        // tensor to return later
+        var to_return = new Array(MAX_LEN).fill(0);
+        var length = seedWord.length;
+        // If the word is in our dictionary we assign it it's value
+        // else it gets "deleted" by the offset
+        var offset = 0;
+        for (var i = 0; i < length; i++) {
+            if (voc.hasOwnProperty(seedWord[i])) {
+                to_return[i - offset] = voc[seedWord[i]]
+            }
+            else {
+                offset = offset + 1;
+            }
+        }
+        const shape = [1, MAX_LEN]
+        // calling the model
+        // var ret = tf.loadLayersModel(MODEL_URL).then(model => {
+        const result = mod.predict(tf.tensor(to_return, shape))
+        var resultData = result.dataSync();
+        var back = resultData[0]
+        //     return back;
+        // });
+        return back;
+}
+
 
 
 
